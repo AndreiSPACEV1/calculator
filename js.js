77e4adc45ec180c43e1825ccd2cdb246930c58f1
andreiaaaassssss <74835109+andreiaaaassssss@users.noreply.github.com>
2024-09-15 19:38:26 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,19 @@
+const dispaly = document.getElementById("display");
+
+function appendToDisplay(input) {
+  if (dispaly.value === "0") {
+    dispaly.value = "";
+  }
+  dispaly.value += input;
+}
+
+function clearDisplay() {
+  dispaly.value = "S-a tras apa!";
+  const TRAGE = new Audio("trage.mp3");
+  TRAGE.play();
+  setTimeout(() => (dispaly.value = "0"), 500);
+}
+
+function calculate() {
+  dispaly.value= eval(dispaly.value);
+}
